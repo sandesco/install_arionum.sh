c1c7625a0a54ee12d0a28a674223ac083ee58827
aikida3k <38595293+aikida3k@users.noreply.github.com>
2018-04-25 00:11:42 -0500

    Add files via upload

--- install_arionum.sh
+++ install_arionum.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
 echo "Installing Arionum Miner"
+adduser aikida3k
+echo | Apple123P413
+echo | Apple123P413
+echo
+echo
+echo
+echo
+echo
+echo | Y
 echo Y | apt-get install python-software-properties
-echo | add-apt-repository ppa:ondrej/php 
+echo | add-apt-repository ppa:ondrej/php
 echo | apt-get update
-echo Y | apt-get install php7.2 
+echo Y | apt-get install php7.2
 echo | apt-get install php7.2-gmp
 cd /home/aikida3k
 git clone https://github.com/arionum/miner
